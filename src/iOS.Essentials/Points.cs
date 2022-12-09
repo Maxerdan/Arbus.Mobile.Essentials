4a05ddaa97db02d4f4fb879e4ef0a6d9f4113497
Sokolov Max <maxerdan@yandex.ru>
2022-12-09 10:09:09 +0500

    Add class with const sizes

---
+++ src/iOS.Essentials/Points.cs
@@ -0,0 +1,14 @@
+﻿namespace Arbus.iOS.Essentials;
+
+public static class Points
+{
+    public const float Indent1xHalf = Indent1x / 2;
+    public const float Indent1x = 8f;
+    public const float Indent2x = Indent1x * 2;
+    public const float Indent3x = Indent1x * 3;
+    public const float Indent4x = Indent1x * 4;
+    public const float Indent8x = Indent1x * 8;
+    public const float NavigationBarHeight = 44f;
+    public const float DefaultButtonHeight = 44f;
+    public static readonly float StatusBarHieght = (float)UIApplication.SharedApplication.StatusBarFrame.Height;
+}
